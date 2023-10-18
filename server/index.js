bf574cdd100911c9ab37b65529ab7a6c0da86be4
Varun-Kolanu <kolanuvarun739@gmail.com>
2023-10-18 20:48:26 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import { ApolloServer } from "@apollo/server";
+import {expressMiddleware} from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import {startStandaloneServer} from "@apollo/server/standalone"
+
+const app = express();
+
+//* Server setup
+const server = new ApolloServer({
+    //typeDefs
+    //resolvers
+})
+
+
+
+
+const {url} = await startStandaloneServer(server, {
+    listen: {
+        port: 8000
+    }
+})
+
+console.log(`Server listening at ${url}`);
